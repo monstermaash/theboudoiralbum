41ab6177e7b6028bd3a57fb6f3ed2994c8985fe6
monstermaash <andy.vo170@hotmail.com>
2024-06-28 11:00:41 -0400

    Add login.css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/login.css', 'resources/js/app.js'])
 </head>
 
 <body>
